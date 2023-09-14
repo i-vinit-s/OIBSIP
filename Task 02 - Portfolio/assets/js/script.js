e3a662eefb87c367e2d472f707c33a3bfe279151
Vinit / <122591051+i-vinit-s@users.noreply.github.com>
2023-09-15 00:23:31 +0530

    Add files via upload

---
+++ Task 02 - Portfolio/assets/js/script.js
@@ -0,0 +1,93 @@
+// Right Click None
+$(document).ready(function () {
+  $("body").on("contextmenu", function (e) {
+    return false;
+  });
+});
+
+// Age Auto Update
+$(document).ready(function () {
+  var dob = new Date("06/06/2005");
+  //calculate month difference from current date in time
+  var month_diff = Date.now() - dob.getTime();
+
+  //convert the calculated difference in date format
+  var age_dt = new Date(month_diff);
+
+  //extract year from date
+  var year = age_dt.getUTCFullYear();
+
+  //now calculate the age of the user
+  var age = Math.abs(year - 1970);
+
+  //display the calculated age
+  $("#exact_age").text(age);
+});
+
+// Hero Bottom Text Animation
+class TextScramble {
+  constructor(el) {
+    this.el = el;
+    this.chars = "!@#$%^&()-_[]{}â€”=+*^?#_";
+    this.update = this.update.bind(this);
+  }
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 40);
+      const end = start + Math.floor(Math.random() * 40);
+      this.queue.push({ from, to, start, end });
+    }
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
+        }
+        output += `<span class="dud">${char}</span>`;
+      } else {
+        output += from;
+      }
+    }
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
+    }
+  }
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
+  }
+}
+const phrases = ["a CSE student", "a web developer"];
+const el = document.querySelector(".bottomText");
+const fx = new TextScramble(el);
+let counter = 0;
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 1000);
+  });
+  counter = (counter + 1) % phrases.length;
+};
+next();
+
+// End
